26167465d6062b58e746eb0d76d64564b0587b18
Ozod <o.bukhorizoda@mail.ru>
2024-03-05 10:03:17 +0500

    add basic cart functionality

--- src/components/Modal/Modal.tsx
+++ src/components/Modal/Modal.tsx
@@ -10,18 +10,55 @@ export const Modal = () => {
       <div className={styles.modal_container}>
         <Typography.Title level={5}>Cart</Typography.Title>
         <Divider style={{ margin: 0 }}></Divider>
-        <Flex
+
+        {cart.length === 0 && (
+          <Flex
+            style={{ width: "100%", marginTop: "7px" }}
+            justify="center"
+            align="center"
+          >
+            <h3>Your cart is empty</h3>
+          </Flex>
+        )}
+        <div
           style={{
+            display: "flex",
             justifyContent: "center",
-            alignItems: "center",
-            height: "188px",
+            flexDirection: "column",
+            marginTop: "7px",
           }}
         >
-          <h3>Your cart is empty</h3>
           {cart?.map((cartItem) => {
-            return <h1>{cartItem.title}</h1>;
+            return (
+              <div
+                style={{
+                  display: "flex",
+                  justifyContent: "start",
+                  alignItems: "center",
+                  gap: "10px",
+                }}
+              >
+                <img
+                  style={{ width: "40px", height: "40px" }}
+                  src={cartItem.img}
+                  alt={cartItem.title}
+                />
+                <div
+                  style={{
+                    display: "flex",
+                    flexDirection: "column",
+                    justifyContent: "center",
+                  }}
+                >
+                  <div style={{ height: 0, marginBottom: "20px" }}>
+                    {cartItem.title}
+                  </div>
+                  <div>$125.00 x 1 $125.00</div>
+                </div>
+              </div>
+            );
           })}
-        </Flex>
+        </div>
       </div>
     </>
   );
