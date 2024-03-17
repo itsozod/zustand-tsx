88655c30729d14d7e694b5834ff6bfa211e11597
Ozod <o.bukhorizoda@mail.ru>
2024-03-17 18:08:26 +0500

    add embla carousel library for swiping

--- src/components/Swiper/Swiper.tsx
+++ src/components/Swiper/Swiper.tsx
@@ -1,36 +1,26 @@
+// import { Button, Flex, Input } from "antd";
+import { useCallback } from "react";
 import styles from "./Swiper.module.css";
-import { Swiper, SwiperSlide } from "swiper/react";
-import {
-  Navigation,
-  Pagination,
-  Scrollbar,
-  A11y,
-  Mousewheel,
-} from "swiper/modules";
-import "swiper/css";
-import "swiper/css/navigation";
-import "swiper/css/pagination";
-import "swiper/css/scrollbar";
-import "swiper/css/mousewheel";
+import useEmblaCarousel from "embla-carousel-react";
 import { Button, Flex, Input } from "antd";
-import { useCartStore } from "../../store";
-
-const product = [
-  {
-    id: 1,
-    images: [
-      "image-product-1.jpg",
-      "image-product-2.jpg",
-      "image-product-3.jpg",
-      "image-product-4.jpg",
-    ],
-    img: "image-product-1.jpg",
-    title: "Fall Limited Edition Sneakers",
-  },
+const images = [
+  "image-product-1.jpg",
+  "image-product-2.jpg",
+  "image-product-3.jpg",
+  "image-product-4.jpg",
 ];
 
 export const SwiperContent = () => {
-  const { addToCart } = useCartStore();
+  const [emblaRef, emblaRefApi] = useEmblaCarousel();
+  const [emblaThumbsRef, emblaThumbsApi] = useEmblaCarousel();
+
+  const onThumbClick = useCallback(
+    (index: number) => {
+      if (!emblaRefApi || !emblaThumbsApi) return;
+      emblaRefApi.scrollTo(index);
+    },
+    [emblaRefApi, emblaThumbsApi]
+  );
   return (
     <>
       <div
@@ -41,83 +31,93 @@ export const SwiperContent = () => {
           width: "100%",
         }}
       >
-        {product.map((product) => (
-          <>
-            <div style={{ width: "400px" }}>
-              <Swiper
-                key={product.id}
-                style={{ borderRadius: "12px" }}
-                modules={[Navigation, Pagination, Scrollbar, A11y, Mousewheel]}
-                spaceBetween={50}
-                slidesPerView={1}
-                navigation
-                pagination={{ clickable: true }}
-                mousewheel={{
-                  releaseOnEdges: true,
-                  thresholdDelta: 1,
-                  thresholdTime: 1,
-                  sensitivity: 1,
-                }}
-                onSwiper={(swiper) => console.log(swiper)}
-                onSlideChange={() => console.log("slide change")}
+        <div style={{ width: "400px" }}>
+          <div className="embla" ref={emblaRef}>
+            {" "}
+            <div className="embla__container">
+              {" "}
+              {images.map((img, i) => {
+                return (
+                  <div key={i} className="embla__slide">
+                    <img style={{ width: "100%" }} src={img} alt="" />
+                  </div>
+                );
+              })}
+            </div>{" "}
+          </div>
+
+          <div className="embla-thumbs" style={{ width: "100%" }}>
+            {" "}
+            <div className="embla-thumbs__viewport" ref={emblaThumbsRef}>
+              <div
+                className="embla-thumbs__container"
+                style={{ display: "flex", width: "93px", gap: "10px" }}
               >
-                {product.images.map((image, index) => (
-                  <SwiperSlide key={index}>
-                    <img style={{ maxWidth: "100%" }} src={image} alt="" />
-                  </SwiperSlide>
-                ))}
-              </Swiper>
+                {images.map((img, i) => {
+                  return (
+                    <div
+                      onClick={() => onThumbClick(i)}
+                      key={i}
+                      className="embla__slide"
+                    >
+                      <img style={{ width: "100%" }} src={img} alt="" />
+                    </div>
+                  );
+                })}
+              </div>{" "}
             </div>
-            <Flex className={styles.contentinfo_container}>
-              <h2 style={{ color: "orange" }}>Sneaker company</h2>
-              <h1>{product.title}</h1>
-              <p>
-                These low-profile sneakers are your perfect casual wear
-                companion. Featuring a durable rubber outer soul, they'll
-                withstand everything the weather will offer.
-              </p>
-              <Flex gap={20}>
-                <div>$125.00</div>
-                <div>50%</div>
-              </Flex>
-              <p>$250.00</p>
-              <Flex gap={10}>
-                <Flex>
-                  <Button
-                    style={{
-                      background: "#f7f7f7",
-                      color: "hsl(26, 100%, 55%)",
-                    }}
-                  >
-                    -
-                  </Button>
-                  <Input
-                    style={{ background: "#f7f7f7", textAlign: "center" }}
-                    placeholder="0"
-                  />
-                  <Button
-                    style={{
-                      background: "#f7f7f7",
-                      color: "hsl(26, 100%, 55%)",
-                    }}
-                  >
-                    +
-                  </Button>
-                </Flex>
-                <Button
-                  onClick={() => addToCart(product)}
-                  style={{
-                    width: "100%",
-                    background: "hsl(26, 100%, 55%)",
-                    color: "#fff",
-                  }}
-                >
-                  Add to cart
-                </Button>
-              </Flex>
+          </div>
+        </div>
+
+        <Flex className={styles.contentinfo_container}>
+          <h2 style={{ color: "orange" }}>Sneaker company</h2>
+          {/* <h1>{product.title}</h1> */}
+          <h1>Fall Limited Sneakers</h1>
+          <p>
+            These low-profile sneakers are your perfect casual wear companion.
+            Featuring a durable rubber outer soul, they'll withstand everything
+            the weather will offer.
+          </p>
+          <Flex gap={20}>
+            <div>$125.00</div>
+            <div>50%</div>
+          </Flex>
+          <p>$250.00</p>
+          <Flex gap={10}>
+            <Flex>
+              <Button
+                style={{
+                  background: "#f7f7f7",
+                  color: "hsl(26, 100%, 55%)",
+                }}
+              >
+                -
+              </Button>
+              <Input
+                style={{ background: "#f7f7f7", textAlign: "center" }}
+                placeholder="0"
+              />
+              <Button
+                style={{
+                  background: "#f7f7f7",
+                  color: "hsl(26, 100%, 55%)",
+                }}
+              >
+                +
+              </Button>
             </Flex>
-          </>
-        ))}
+            <Button
+              // onClick={() => addToCart(product)}
+              style={{
+                width: "100%",
+                background: "hsl(26, 100%, 55%)",
+                color: "#fff",
+              }}
+            >
+              Add to cart
+            </Button>
+          </Flex>
+        </Flex>
       </div>
     </>
   );
