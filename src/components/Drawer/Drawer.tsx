36794e83fce8a1eeca1ad6ff365af86928c78bc2
Ozod <o.bukhorizoda@mail.ru>
2024-03-22 15:15:43 +0500

    create a drawer

--- src/components/Drawer/Drawer.tsx
+++ src/components/Drawer/Drawer.tsx
@@ -1,4 +1,4 @@
-import { Drawer } from "antd";
+import { Drawer, Flex } from "antd";
 
 export const DrawerContent = ({
   open,
@@ -13,8 +13,31 @@ export const DrawerContent = ({
         open={open}
         onClose={onClose}
         placement="left"
-        title="Hello World"
-      ></Drawer>
+        title={null}
+        footer={null}
+        styles={{
+          body: { padding: 0 },
+          header: { borderBottom: "none", padding: "16px 26px" },
+        }}
+      >
+        <Flex vertical={true} justify="start" align="start" style={{marginTop: "15px"}}>
+          <ul
+            style={{
+              display: "flex",
+              flexDirection: "column",
+              justifyContent: "start",
+              alignItems: "start",
+              gap: "10px",
+            }}
+          >
+            <li>Collections</li>
+            <li>Men</li>
+            <li>Women</li>
+            <li>About</li>
+            <li>Contact</li>
+          </ul>
+        </Flex>
+      </Drawer>
     </>
   );
 };
